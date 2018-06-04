735d80f7fc0fb0a556a6f8987f2f75847ff3a9a8
gesanduo <gesanduo@163.com>
2018-06-04 20:22:48 +0800

    优化

--- index.js
+++ index.js
@@ -4,8 +4,28 @@ $(function(){
         $('.index .h-content').addClass('hidden').eq(index).removeClass('hidden');
         $('.index .main').addClass('hidden').eq(index).removeClass('hidden');
         $(this).addClass('current').siblings().removeClass('current');
+        localStorage.setItem('yiboIndex',index);
     })
     $(".main .swiper-container").swiper({
         autoplay:3000
+    });
+    if(localStorage.getItem('yiboIndex')==1){
+        var index =  parseInt(localStorage.getItem('yiboIndex'));
+        $('.index .h-content').addClass('hidden').eq(index).removeClass('hidden');
+        $('.index .main').addClass('hidden').eq(index).removeClass('hidden');
+        $('.footer li').eq(index).addClass('current').siblings().removeClass('current');
+        $('.yibo').removeClass('hidden');
+    }else{
+        var index = 0;
+        $('.index .h-content').addClass('hidden').eq(index).removeClass('hidden');
+        $('.index .main').addClass('hidden').eq(index).removeClass('hidden');
+        $('.footer li').eq(index).addClass('current').siblings().removeClass('current');
+        $('.yibo').removeClass('hidden');
+    }
+    $('.bind_bank').on('click',function(){
+         $('.js-car-mask').removeClass('hidden');
+    })
+    $('.js-car-mask').on('click',function(){
+        //前往綁卡支付
     });
 })
